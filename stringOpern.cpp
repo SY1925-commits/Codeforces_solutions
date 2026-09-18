75113161b81236a34d2e63ddcd849519d1607724
S$Y <syedyasser728@gmail.com>
2026-09-18 18:15:39 +0530

    problem_59A

---
+++ stringOpern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+   string s; 
+   int upper = 0, lower = 0;
+   cin >> s;
+   for(int i=0; i<s.length(); i++)
+      if(isupper(s[i]))
+         upper++;
+      else
+         lower++;
+   if(upper>lower)
+      for(int i=0; i<s.length(); i++)
+         s[i] = toupper(s[i]);
+   else
+      for(int i=0; i<s.length(); i++)
+         s[i] = tolower(s[i]);
+   cout << s;
+   return 0;
+}
